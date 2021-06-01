539b70a117c2566e7dae5a6ed77d603305cdc1e7
alekyaagadiraju <74793945+alekyaagadiraju@users.noreply.github.com>
2021-05-31 21:35:09 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -53,17 +53,7 @@ def upload_files():
 if __name__ == '__main__':
    app.run(debug = True)   
 
-img = cv2.imread(r'letter_m.jpg')
-img_copy = img.copy()
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img = cv2.resize(img, (400,440))
-img_copy = cv2.GaussianBlur(img_copy, (7,7), 0)
-img_gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
-_, img_thresh = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY_INV)
-
-img_final = cv2.resize(img_thresh, (28,28))
-img_final =np.reshape(img_final, (1,28,28,1))
 
 
 
