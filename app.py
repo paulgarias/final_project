e6a9ad3f73467692c677676f5a65ed3cd1cd2e62
alekyaagadiraju <74793945+alekyaagadiraju@users.noreply.github.com>
2021-05-31 20:08:33 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ import numpy as np
 
 app = Flask(__name__)
 
-model = keras.models.load_model('model_hand.h5')
+model = tf.keras.models.load_model('model_hand.h5')
+
+letters = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X', 24:'Y',25:'Z'}
 
 @app.route('/upload')
 def upload_file():
@@ -20,29 +22,46 @@ def upload_files():
    if request.method == 'POST':
       f = request.files['file']
       f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+      # return 'file uploaded successfully'
 
-      # # # Read from file and convert to tensor
-      # img_tensor = keras.io.decode_image(filename)
+      img = cv2.imread(f.filename)
+      img_copy = img.copy()
+
+      img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+      img = cv2.resize(img, (400,440))
+
+
+      img_copy = cv2.GaussianBlur(img_copy, (7,7), 0)
+
+      img_gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
+      _, img_thresh = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY_INV)
+
+      img_final = cv2.resize(img_thresh, (28,28))
+      img_final =np.reshape(img_final, (1,28,28,1))
+
+      img_pred = letters[np.argmax(model.predict(img_final))]
+      return f"The letter is : {str(results)}\n\n"
+
+      # # # Read from file
+      # img_tensor = tf.io.decode_image(f.filename)
       # results = model.predict(img_tensor)
 
-        # Delete the file
-      # os.remove(filename)
-      return "The letter is : "+str(results)+"\n\n"
+      # #   Delete the file
+      # os.remove(f.filename)
+      # return f"The letter is : {str(results)}\n\n"
 
 if __name__ == '__main__':
    app.run(debug = True)   
 
 
-      img = 
 
 
-      # #read image file string data
-      # f = request.files['file'].read()
-      # #convert string data to numpy array
-      # npimg = numpy.fromstring(f, numpy.uint8)
-      # # convert numpy array to image
-      # img = cv2.imdecode(npimg, cv2.CV_LOAD_IMAGE_UNCHANGED)
+
+
+
+
+
+      
          
 
 
